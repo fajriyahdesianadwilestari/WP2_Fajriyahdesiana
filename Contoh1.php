42ff7b3a15e7e93ff4fa80fd74b0afdb80417b62
fajriyahdesianadwilestari <91240528+fajriyahdesianadwilestari@users.noreply.github.com>
2021-09-23 23:24:19 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h2>Perkenalkan</h2>";
+        echo"Nama saya Fajriyah Desiana Dwi Lestari
+            Saya tinggal di daerah Cimanggu Bogor<br>
+            Saya Mahasiswa Semester 3 Universitas <br>
+            Bina Sarana Informatika Jurusan Sistem 
+            Informasi Kelas 13.3A.13"; 
+    }
+}
